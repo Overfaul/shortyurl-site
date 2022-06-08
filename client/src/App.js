7ca10a9c1054c2ea270aee9a84decf356836eccd
Nikita <saverka5@gmail.com>
2022-06-08 12:50:07 +0300

    changed footer height

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import MainPage from './page/MainPage';
-import PrivacyPage from './page/PrivacyPage'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './styles/App.css';
 function App() {
